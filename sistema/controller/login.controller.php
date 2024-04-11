91a9f26611d89298b1789cd89e9ea18795a9da26
Álvaro Carvalho <alvaro.carvalhohdr@gmail.com>
2024-04-11 00:30:09 -0300

    refactory-login

--- sistema/controller/login.controller.php
+++ sistema/controller/login.controller.php
@@ -1,24 +1,20 @@
 <?php
 require_once $_SERVER["DOCUMENT_ROOT"] . '/sistema/controller/global.controller.php';
-require_once $_SERVER["DOCUMENT_ROOT"] . '/sistema/model/pdo.model.php';
 require_once $_SERVER["DOCUMENT_ROOT"] . '/sistema/model/login.model.php';
 require_once $_SERVER["DOCUMENT_ROOT"] . '/sistema/controller/login.service.php';
 
 if (!empty($_POST['email']) && validateForm($_POST)) {
     $login = new Login($_POST['email'], $_POST['senha']);
-    $conexao = new Conexao();
     $loginService = new LoginService($conexao, $login);
-
     if ((bool)count($loginService->validaLogin())) {
         $loginService->iniciarSessao($loginService->validaLogin()[0]->id);
-        header('Location: /sistema/index.php');
+        header('Location: /sistema/index.php?login=aceito');
     } else {
         header('Location: /sistema/view/login.php?failLogin=true');
     }
 }
 
 if (isset($_POST['endSession'])) {
-    $conexao = new Conexao();
     $loginService = new LoginService($conexao);
     $loginService->encerrarSessao();
 }
