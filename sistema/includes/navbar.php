f15f23fb30fd14dd1dbd024831608b23b8a897a7
Álvaro Carvalho <alvaro.carvalhohdr@gmail.com>
2024-05-22 00:12:16 -0300

    notificacao-css-btn-follow-navbar-superior

--- sistema/includes/navbar.php
+++ sistema/includes/navbar.php
@@ -5,14 +5,12 @@
             <i class="fas fa-align-left"></i>
             <span id="label-navbar">Esconder</span>
         </button>
-        <button type="button" class="btn btn-dark ml-2" data-toggle="modal" data-target="#modalFollowup">
-            <i class="fas fa-address-card" id="ico-btn-flw"></i>
-            <span id="span-btn-flw">Followup</span>
-
-            <?php if((bool)count($conexao->criarNotifFollow())) : ?>
-                <h3>NOTIFICAR</h3>
-            <?php endif; ?>
-        </button>
+        <div class="<?= ((bool)count($conexao->criarNotifFollow())) ? 'wrap' : ''; ?>">
+            <button type="button" class="btn btn-dark ml-2 button" data-toggle="modal" data-target="#modalFollowup">
+                <i class="fas fa-address-card" id="ico-btn-flw"></i>
+                <span id="span-btn-flw">Followup</span>
+            </button>
+        </div>
         <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <i class="fas fa-align-justify"></i>
         </button>
