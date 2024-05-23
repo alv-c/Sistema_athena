778f14037ac69a799150d048af621acc92385fc2
Álvaro Carvalho <alvaro.carvalhohdr@gmail.com>
2024-05-22 23:49:42 -0300

    obrigatoriedade-comentario-update-lead

--- sistema/view/edit.php
+++ sistema/view/edit.php
@@ -123,9 +123,9 @@ if ((!empty($_POST['editId']) && !is_null($_POST['editId'])) || (!empty($_GET['e
                                     </div>
                                     <div class="col-md-6 col-sm-12">
                                         <label for="anotacao">Anotação</label>
-                                        <textarea name="anotacao" id="anotacao" placeholder="Comentário"></textarea>
+                                        <textarea name="anotacao" id="anotacao" placeholder="Comentário" class="required-js" required="false"></textarea>
                                         <div class="aviso-follow" id="aviso-follow">
-                                            <div class="alert alert-warning mt-3 role=" alert">
+                                            <div class="alert alert-warning mt-3 role=" role="alert">
                                                 <strong>Atenção!</strong> Esta anotação será utilizada para a criação de follow-up!
                                             </div>
                                         </div>
